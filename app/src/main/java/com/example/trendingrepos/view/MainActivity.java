bff6864f4294fa183ccc1d72e67ca10b047e1132
adnaz <adnanaitzait@gmail.com>
2020-02-27 13:26:26 +0100

    Displaying the Paged List setting up recyclerview, getting our ItemViewModel, creating the Adapter, observing the itemPagedList from view model, setting the adapter for the recycleview

---
+++ app/src/main/java/com/example/trendingrepos/view/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.trendingrepos.view;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.paging.PagedList;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import com.example.trendingrepos.R;
+import com.example.trendingrepos.recyclerview.ItemAdapter;
+import com.example.trendingrepos.response.Item;
+import com.example.trendingrepos.view_model.ItemViewModel;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //setting up recyclerview
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+
+        //getting our ItemViewModel
+        ItemViewModel itemViewModel = new ViewModelProvider(this).get(ItemViewModel.class);
+
+        //creating the Adapter
+        final ItemAdapter adapter = new ItemAdapter(this);
+
+
+        //observing the itemPagedList from view model
+        itemViewModel.itemPagedList.observe(this, new Observer<PagedList<Item>>() {
+            @Override
+            public void onChanged(@Nullable PagedList<Item> items) {
+
+                //in case of any changes
+                //submitting the items to adapter
+                adapter.submitList(items);
+            }
+        });
+
+        //setting the adapter
+        recyclerView.setAdapter(adapter);
+    }
+}
